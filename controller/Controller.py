7a0fe6a6c90107b8839e837b3777b2094c166d1a
Mats-HJalmar Murman <matte.hj1234@gmail.com>
2020-12-11 16:15:08 +0100

    improved with if channel null

--- controller/Controller.py
+++ controller/Controller.py
@@ -10,23 +10,28 @@ class Controller():
             if after.channel == before.channel:
                 return
         except:
-            print(member + " did not change channel")
+            print(member.name + " did not change channel")
         if after is not None:
-            try:
-                await self.controller.checkJoinChannel(member, after.channel)
-                print("Member joined " + after.channel.name)
-            except:
-                print("Member moved to a channel i cant see")
+            if after.channel is not None:
+                ch = await self.client.fetch_channel(after.channel.id)
+                try:
+                    await self.controller.checkJoinChannel(member, ch)
+                    print(member.name + " joined " + ch)
+                except:
+                    print(member.name + " moved to a channel i cant see")
         if before is not None:
+            if before.channel is not None:
+                ch = await self.client.fetch_channel(before.channel.id)
+                try:
+                    deleted = await self.controller.tryDeleteChannel(ch)
+                    if deleted:
+                        print(member.name + " left " + ch.name + " (" + str(len(ch.members)) + "/" + str(ch.user_limit) + ") -> deleting it.")
+                    else:
+                        print(member.name + " left " + ch.name + " (" + str(len(ch.members)) + "/" + str(ch.user_limit) + ") -> not deleting it.")
+                except:
+                    print(member.name + " came from a channel i cant see")
             try:
-                deleted = await self.controller.tryDeleteChannel(before.channel)
-                if deleted:
-                    print("Member left " + before.channel.name + " (" + str(len(before.channel.members)) + "/" + str(before.channel.user_limit) + ") -> deleting it.")
-                else:
-                    print("Member left " + before.channel.name + " (" + str(len(before.channel.members)) + "/" + str(before.channel.user_limit) + ") -> not deleting it.")
+                await self.controller.sortChannels(before.channel.category)
             except:
-                print("Member came from a channel i cant see")
-        try:
-            await self.controller.sortChannels(before.channel.category)
-        except:
-            await self.controller.sortChannels(after.channel.category)
+                await self.controller.sortChannels(after.channel.category)
+            
